7563ac993461bd9c39647fdf7a038255abcf46be
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-04-16 17:25:35 +0100

    Updated Spotify redirect URI

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,7 @@
 import spotifyToken from "./config";
 
 const clientID = spotifyToken;
-const redirectURI = 'http://localhost:3000/';
+const redirectURI = 'https://scottkennethjackson-mixtape.netlify.app/';
 
 let accessToken;
 
