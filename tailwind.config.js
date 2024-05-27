12d470875721d68c3b24272c91196ae4b8be25be
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2024-05-27 14:10:03 +0100

    Add custom font and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,30 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      spacing: {
+        '10%': '10%',
+        '15%': '15%'
+      },
+
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'background-img': "url('/public/images/background.jpg')"
+      },
+
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'darkgray': '#282c34',
+        'darkgrey': '#282c34',
+        'lightblue': '#61dafb'
+      },
+
+      fontFamily: {
+        'marker': ['var(--marker)', 'sans-serif'],
+        'work': ['var(--work)', 'sans-serif']
       },
     },
   },
