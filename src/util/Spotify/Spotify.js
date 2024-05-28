04e758df5f9d29fb21d122c500d08fc8e6f59fd2
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2024-05-28 21:26:50 +0100

    Create Spotify access token

---
+++ src/util/Spotify/Spotify.js
@@ -0,0 +1,25 @@
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) return accessToken;
+
+        const tokenInURL = window.location.href.match(/access_token=([^&]*)/);
+        const expiryTime = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (tokenInURL && expiryTime) {
+            accessToken = tokenInURL[1];
+            const expiresIn = Number(expiryTime[1]);
+
+            window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
+            window.history.pushState('Access token', null, '/');
+
+            return accessToken;
+        };
+
+        const redirect = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUrl}`;
+        window.location = redirect;
+    }
+};
+
+export { Spotify };
