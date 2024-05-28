eb0647d210ccfb5ed23115eb849042d98c79273a
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2024-05-28 09:59:42 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import App from './components/App/App';
+import React from "react";
+import App from "./components/App/App";
 
 export default function Home() {
   return (
-    <main className=''>
+    <main>
       <App />
     </main>
   );
-}
+};
