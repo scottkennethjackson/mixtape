6935204b56c2b9648b053fd259431884d5adbab8
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2024-05-31 12:26:38 +0100

    Update Spotify.config.js

--- src/util/Spotify/Spotify.config.js
+++ src/util/Spotify/Spotify.config.js
@@ -1,2 +1,2 @@
-const clientID = '3e9e90bf1d574d6a964f851e00addc46';
-const redirectUrl = 'https://scottkennethjackson-mixtape.vercel.app/';
+export const clientID = '3e9e90bf1d574d6a964f851e00addc46';
+export const redirectUrl = 'https://scottkennethjackson-mixtape.vercel.app/';
