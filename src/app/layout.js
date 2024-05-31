11ac4ef57411fcad688a8b697e1c32a28fa7004d
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2024-05-31 17:27:13 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-darkgrey">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
