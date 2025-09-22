da60278bd4480b5266a56641fdc0da099234f5cd
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2025-09-22 10:27:12 +0100

    Fix typo

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=" qantialiased">
+      <body className="antialiased">
         {children}
         <Analytics />
       </body>
